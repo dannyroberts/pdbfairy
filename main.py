e31898bedd9174ae3a1d6ba02d6c7fb8bbc237f5
Daniel Roberts <droberts@dimagi.com>
2019-10-04 23:04:19 -0500

    Speed up with 3D atom bucketing

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 
 from Bio import PDB
 from numpy import linalg
+import numpy
 
 
 MAX_DISTANCE = 4
@@ -17,24 +18,28 @@ def main(structure):
 
 def find_pairs(structure, max_distance=MAX_DISTANCE):
     atoms = list(structure.get_atoms())
-    atoms_by_i = collections.defaultdict(list)
+    atoms_by_cube = collections.defaultdict(set)
+    atoms_by_chain = collections.defaultdict(set)
 
-    def get_i(atom):
-        return math.floor(atom.coord[0] / MAX_DISTANCE)
-
-    for atom in atoms:
-        atoms_by_i[get_i(atom)].append(atom)
+    def get_cube(atom):
+        return numpy.floor(atom.coord / max_distance)
 
     for atom in atoms:
-        i = get_i(atom)
+        atoms_by_cube[tuple(get_cube(atom))].add(atom)
+        atoms_by_chain[atom.parent.parent].add(atom)
+
+    for atom in atoms:
+        cube = get_cube(atom)
+        atoms_in_chain = atoms_by_chain[atom.parent.parent]
         yield from (
             (atom, a)
-            for offset in [-1, 0, 1]
-            for a in atoms_by_i[i + offset]
-            if a.parent.parent != atom.parent.parent
-            and dist(a, atom) <= max_distance
+            for i in [-1, 0, 1]
+            for j in [-1, 0, 1]
+            for k in [-1, 0, 1]
+            for a in atoms_by_cube[tuple(cube + numpy.array([i, j, k]))] - atoms_in_chain
+            if dist(a, atom) <= max_distance
         )
-        atoms_by_i[i].remove(atom)
+        atoms_by_cube[tuple(cube)].remove(atom)
 
 
 def dist(atom_1, atom_2):
