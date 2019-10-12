4d513f6f919ade6c98e481c4cbf50aa8d7576833
Daniel Roberts <droberts@dimagi.com>
2019-10-11 23:50:57 -0500

    Run pycodestyle

--- pdbfairy/utils.py
+++ pdbfairy/utils.py
@@ -17,12 +17,12 @@ def capture():
     """
     Thank you https://stackoverflow.com/a/10743550
     """
-    oldout,olderr = sys.stdout, sys.stderr
+    oldout, olderr = sys.stdout, sys.stderr
     try:
-        out=[io.StringIO(), io.StringIO()]
-        sys.stdout,sys.stderr = out
+        out = [io.StringIO(), io.StringIO()]
+        sys.stdout, sys.stderr = out
         yield out
     finally:
-        sys.stdout,sys.stderr = oldout, olderr
+        sys.stdout, sys.stderr = oldout, olderr
         out[0] = out[0].getvalue()
         out[1] = out[1].getvalue()
